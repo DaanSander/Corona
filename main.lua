297d11bf149a086106e59205d44418df1b69375b
Daan <Daan@eriknoel.nl>
2015-10-31 11:07:02 +0100

    Commit

--- main.lua
+++ main.lua
@@ -5,9 +5,15 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
-local background = display.newImage("ding.png")
+-- local background = display.newImage("ding.png")
 
-local myText = display.newText("Hello World ", display.contentWidth / 2, display.contentWidth / 4, native.systemFont, 40)
---myText.x = display.contentWidth / 2
---myText.y = display.contentHeight / 2
-myText:setTextColor(255, 110, 110)
+-- local myText = display.newText("Hello World ", display.contentWidth / 2, display.contentWidth / 4, native.systemFont, 40)
+-- --myText.x = display.contentWidth / 2
+-- --myText.y = display.contentHeight / 2
+-- myText:setTextColor(255, 110, 110)
+-- background.y = display.contentHeight / 2;
+-- print("STENEN " .. background.x)
+local c1 = display.newImage("rondje.png");
+c1.x = c1.x + 40;
+c1.y = c1.y + 40;
+local c2 = display.newImage("Rondje2.png");
